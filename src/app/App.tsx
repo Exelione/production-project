c7b83d62198fe181d1a5ac08ad3d01adff7381d2
Exelione <exelione@gmail.com>
2025-01-17 04:36:27 +0300

    navbarComponent

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,12 +1,10 @@
 
-import { Route, Routes } from 'react-router-dom'
 import './styles/index.scss'
 import { Link } from 'react-router-dom'
-import { Suspense } from 'react'
-import { classNames } from 'shared/lib/classNames'
+import { classNames } from 'shared/lib/classNames/classNames'
 import { useTheme } from 'app/providers/ThemeProvider'
-import { MainPage } from 'pages/MainPage/'
-import { AboutPage } from 'pages/AboutPage/'
+import { AppRouter } from './providers/router'
+import { Navbar } from 'widgets/Navbar'
 
 
 
@@ -15,15 +13,9 @@ const App = () => {
     const { theme, toggleTheme } = useTheme();
     return (
         <div className={classNames('app', {}, [theme])}>
+            <Navbar />
+            <AppRouter />
             <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={"/"}>Главная</Link>
-            <Link to={"/about"}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={"/about"} element={<AboutPage />} />
-                    <Route path={"/"} element={<MainPage />} />
-                </Routes>
-            </Suspense>
         </div>
     )
 }
