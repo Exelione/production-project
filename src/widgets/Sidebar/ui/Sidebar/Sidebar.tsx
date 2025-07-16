a254b5b1f211c15976b57df6955c2200ebad7eff
Exelione <exelione@gmail.com>
2025-07-16 23:55:18 +0300

    feat/10:addReactTestingLibAddButtonAndSidebarTests

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import LangSwitcher from 'widgets/LangSwitcher/LangSwitcher';
 import cls from './Sidebar.module.scss';
+import { useTranslation } from 'react-i18next';
 
 interface SidebarProps {
     className?: string;
@@ -10,14 +11,17 @@ interface SidebarProps {
 
 export const Sidebar = ({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false);
+    const { t } = useTranslation('');
     const onToggle = () => {
         setCollapsed((prev) => !prev);
     };
     return (
 
-        <div className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}>
+        <div
+            data-testid="sidebar"
+            className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}>
 
-            <button type="button" onClick={onToggle}>toggle</button>
+            <button data-testid="openBtn" type="button" onClick={onToggle}>{t('Open')}</button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
